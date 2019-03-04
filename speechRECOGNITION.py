2da572ca85e5d7781c5f2214f28025dc175117b7
Farhanullah Shariff <44924778+farhanshariff@users.noreply.github.com>
2019-03-04 09:46:28 +0530

    Add files via upload

---
+++ speechRECOGNITION.py
@@ -0,0 +1,20 @@
+#start off with "(sudo) pip install SpeechRecognition "
+#the audio file recorded has to be of the extention ".wav"
+#save the audio file in the same folder where the program is stored and run the program
+
+
+import speech_recognition as sr
+audio_file=("test1.wav")    #test1.wav is the name of the audio file
+
+r=sr.Recognizer()          #initialize recognizer
+
+with sr.AudioFile(audio_file) as source:
+    audio=r.record(source)         #reads audio file
+try:
+    print("audio file contains " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("sorry, audio cant be understood")
+except sr.RequestError:
+    print("no results from google speech recognition")
+
+
